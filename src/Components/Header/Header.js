5cff7d8977e12176ea6ca6ed3729f0539461db13
Christopher Booth <christopher.booth@juliusbaer.com>
2023-09-19 14:44:41 +0200

    Pulling out components

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,11 +1,13 @@
-﻿import './Header.css';
-import logo from '../../assets/investment-calculator-logo.png';
+﻿import styles from "./Header.module.css";
+import logo from "../../assets/investment-calculator-logo.png";
 
 const Header = () => {
-    return ( <header className="header">
-        <img src={logo} alt="logo" />
-        <h1>Investment Calculator</h1>
-    </header>);
-}
+  return (
+    <header className={styles.header}>
+      <img src={logo} alt="logo" />
+      <h1>Investment Calculator</h1>
+    </header>
+  );
+};
 
 export default Header;
---
+++ src/Components/Results/Result.js
@@ -0,0 +1,28 @@
+﻿import styles from "./Results.module.css";
+
+const Result = (props) => {
+  return (
+    <table className={styles.result}>
+      <thead>
+        <tr>
+          <th>Year</th>
+          <th>Total Savings</th>
+          <th>Interest (Year)</th>
+          <th>Total Interest</th>
+          <th>Invested Capital</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>YEAR NUMBER</td>
+          <td>TOTAL SAVINGS END OF YEAR</td>
+          <td>INTEREST GAINED IN YEAR</td>
+          <td>TOTAL INTEREST GAINED</td>
+          <td>TOTAL INVESTED CAPITAL</td>
+        </tr>
+      </tbody>
+    </table>
+  );
+};
+
+export default Result;
